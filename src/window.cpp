3b323ab4d9252b8c56b79d54021077651fd63cb7
Alexandru Ica <alexandruica703@gmail.com>
2018-12-05 19:52:31 +0200

    Realised I didn't call SDL_Init.

--- src/window.cpp
+++ src/window.cpp
@@ -7,24 +7,31 @@ hw::window::window(const int t_width, const int t_height, const char* t_name)
     , m_renderer(nullptr)
     , m_width(t_width)
     , m_height(t_height)
-    , m_events(nullptr)
+   // , m_events(nullptr)
+    , m_event_queue(std::vector<SDL_Event>{})
     , m_color{ 0, 0, 0, 255 }
 {
+    SDL_Init(SDL_INIT_VIDEO);
+        
     m_window = SDL_CreateWindow(t_name, 
             SDL_WINDOWPOS_UNDEFINED,
             SDL_WINDOWPOS_UNDEFINED,
             m_width, m_height,
             SDL_WINDOW_RESIZABLE);
 
+    SDL_SetWindowResizable(m_window, SDL_FALSE);
+
     m_renderer = SDL_CreateRenderer(m_window, -1, 
-            SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            SDL_RENDERER_ACCELERATED /*| SDL_RENDERER_PRESENTVSYNC*/);
+
+    m_event_queue.reserve(10);
 
     if(!m_window) {
         SDL_Log("Could not initialize window %s \n", SDL_GetError());
     }
 
-    SDL_PumpEvents();
-    m_events = SDL_GetKeyboardState(nullptr);
+    //SDL_PumpEvents();
+    //m_events = SDL_GetKeyboardState(nullptr);
 }
 
 hw::window::~window()
@@ -38,8 +45,19 @@ hw::window::~window()
 void hw::window::update()
 {
     SDL_RenderPresent(m_renderer);
+    handle_events();
 
-    m_events = SDL_GetKeyboardState(nullptr);
+    //m_events = SDL_GetKeyboardState(nullptr);
+}
+
+void hw::window::handle_events()
+{
+    m_event_queue.clear();
+
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+        m_event_queue.push_back(event);
+    }
 }
 
 void hw::window::set_bgcolor(const std::uint8_t t_r, const std::uint8_t t_g,
@@ -60,22 +78,63 @@ void hw::window::set_bgcolor(hw::color&& t_color)
 
 void hw::window::clear()
 {
-    SDL_SetRenderDrawColor(m_renderer, m_color.r, m_color.g, m_color.b, m_color.a);
+    SDL_SetRenderDrawColor(m_renderer, 
+            m_color.r, m_color.g, m_color.b, m_color.a);
     SDL_RenderClear(m_renderer);
 
-    SDL_PumpEvents();
+    //SDL_PumpEvents();
 }
 
 bool hw::window::was_key_pressed(int t_key)
 {
-    return m_events[t_key];
+    //return m_events[t_key];
+    
+    //SDL_Event event;
+    //while(SDL_PollEvent(&event)) {
+    //    switch(event.type) 
+    //    {
+    //        case SDL_KEYDOWN:
+    //            return t_key == event.key.keysym.sym; 
+    //        default:
+    //            break;
+    //    }
+    //}
+
+    //return false;
+    //
+    for(const auto& event : m_event_queue) {
+        switch(event.type)
+        {
+            case SDL_KEYDOWN:
+                return t_key == event.key.keysym.sym;
+            default:
+                break;
+        }
+    }
+
+    return false;
 }
 
-bool hw::window::closed(hw::window::event_t& t_event)
+bool hw::window::closed()
 {
-    while(SDL_PollEvent(&t_event)) {
-        if(t_event.type == SDL_QUIT) {
-            return true;
+    //SDL_Event event;
+    //while(SDL_PollEvent(&event)) {
+    //    switch(event.type) {
+    //        case SDL_QUIT: 
+    //            return true;
+    //        default:
+    //            break;
+    //    }
+    //}
+
+    //return false;
+    for(const auto& event : m_event_queue) {
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                return true;
+            default: 
+                break;
         }
     }
 
