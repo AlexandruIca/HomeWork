25c7f994fc63d41663b74a9b6e5c46325f40e2df
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-04-30 22:40:12 +0300

    Even though I format in vim before I save a file it's still useful to format all files of the project.

---
+++ scripts/run_clang_format.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import os
+import subprocess
+import argparse
+import sys
+
+directories_to_run_through = ["../src/", "../include"]
+
+for directory in directories_to_run_through:
+    files_for_clang_tidy = os.listdir(directory)
+
+    for curr_file in files_for_clang_tidy:
+        full_path = os.path.join(directory, curr_file)
+        print("Running clang-format for: ", full_path)
+        subprocess.run("clang-format -i " + full_path, shell=True)
+
