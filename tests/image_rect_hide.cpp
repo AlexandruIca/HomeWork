4974a8d691dc7a0432729744d867d6645b3eb076
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-05-05 19:08:31 +0300

    Hiding rectangle of an image should cause no problems.

---
+++ tests/image_rect_hide.cpp
@@ -0,0 +1,21 @@
+#include "graphics.hpp"
+
+int main()
+{
+    set_background(YELLOW);
+
+    OutlineRectangle rect{100, 100, 200, 300, CYAN};
+    Image img{"../media/Triangle.png", rect};
+
+    rect.hide();
+
+    return draw(WITH {
+        if(key(KEY_UP)) {
+            rect.pos().y -= 10;
+        }
+        if(key(KEY_DOWN)) {
+            rect.pos().y += 10;
+        }
+    });
+}
+
