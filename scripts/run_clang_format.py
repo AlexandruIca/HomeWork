518867438fa1c910a14c183c67349ba10057c3d7
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-05-17 20:02:25 +0300

    Refactored the code for scripts.
    Now when the scripts are running on a certain file that file is pretty
    printed with color using colorama. Also renamed some functions and
    variables and now the scripts are using type hints for "modernizing"
    them.

--- scripts/run_clang_format.py
+++ scripts/run_clang_format.py
@@ -2,14 +2,31 @@
 
 import os
 import subprocess
+# pip3 install colorama
+import colorama
+from typing import List
 
-directories_to_run_through = ["../src/", "../include"]
+def absolute_path(path: str) -> str:
+    return os.path.abspath(path)
+
+def join(path1: str, path2: str) -> str:
+    return os.path.join(path1, path2)
+
+parent_dir: str = absolute_path("../")
+directories_to_run_through: List[str] =\
+        [join(parent_dir, "src/"), join(parent_dir, "include/")]
+
+colorama.init()
 
 for directory in directories_to_run_through:
-    files_for_clang_tidy = os.listdir(directory)
+    files_for_clang_format: List[str] = os.listdir(directory)
 
-    for curr_file in files_for_clang_tidy:
-        full_path = os.path.join(directory, curr_file)
-        print("Running clang-format for: ", full_path)
+    for curr_file in files_for_clang_format:
+        full_path: str = join(directory, curr_file)
+        print(colorama.Fore.CYAN, end="")
+        print("Running clang-format for: ", end="")
+        print(colorama.Fore.YELLOW, full_path, colorama.Fore.RESET)
         subprocess.run("clang-format -i " + full_path, shell=True)
+
+colorama.deinit()
 
