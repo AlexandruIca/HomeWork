5c5e820ff131efcbc01b81880878eda397116369
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-05-22 19:18:11 +0300

    Test the new mouse functionality.

---
+++ tests/mouse.cpp
@@ -0,0 +1,32 @@
+#include "graphics.hpp"
+
+#include <iostream>
+
+void update(float const t_time)
+{
+    static float time = 0.f;
+
+    time += t_time;
+
+    if(time >= 0.5f) {
+        std::cout << mouse().x << ' ' << mouse().y << '\n';
+        time = 0.f;
+    }
+
+    if(left_click()) {
+        std::cout << "Left click!" << std::endl;
+    }
+    if(right_click()) {
+        std::cout << "Right click!" << std::endl;
+    }
+}
+
+int main()
+{
+    std::cout << "Should not crash: " << mouse().x << ' ' << mouse().y
+              << std::endl;
+    std::cout << "Also should not crash: " << is_left_click() << ' '
+              << is_right_click() << std::endl;
+    std::cout << "PERFECT!" << std::endl;
+    return draw(update);
+}
