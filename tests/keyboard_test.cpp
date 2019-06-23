c88c36a723c9bbfb998d81b49a65a78663b0e2d2
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-06-23 18:39:23 +0300

    Shouldn't test the internal API but the end-user one.

--- tests/keyboard_test.cpp
+++ tests/keyboard_test.cpp
@@ -2,14 +2,17 @@
 
 #include "graphics.hpp"
 
+void update(float const)
+{
+    if(key(KEY_a)) {
+        std::cout << "key 'a' pressed" << std::endl;
+    }
+}
+
 int main()
 {
-    hw::window test_window{1280, 720, "Test Window"};
+    set_width(1280);
+    set_height(720);
 
-    while(!test_window.closed() && !test_window.was_key_pressed(KEY_ESCAPE)) {
-        if(test_window.was_key_pressed(KEY_a)) {
-            std::cout << "key 'a' pressed" << std::endl;
-        }
-        test_window.update();
-    }
+    return draw(update);
 }
